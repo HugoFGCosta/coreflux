3299e9047d71a739132447a002776bb9a5e03950
HugoFGCosta <hugo.costa.T0126211@edu.atec.pt>
2024-09-29 17:24:13 +0100

    structured footer

--- script.js
+++ script.js
@@ -99,3 +99,71 @@ $(document).ready(function () {
 });
 
 //fim de slick slider
+
+//slick slider para mobile nos cards
+$(document).ready(function () {
+  function slickInit() {
+    if ($(window).width() < 768) {
+      if (!$(".cards-container").hasClass("slick-initialized")) {
+        $(".cards-container").slick({
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1.1,
+          slidesToScroll: 1,
+          variableWidth: false,
+          arrows: false,
+          centerMode: false,
+        });
+      }
+    } else {
+      if ($(".cards-container").hasClass("slick-initialized")) {
+        $(".cards-container").slick("unslick");
+      }
+    }
+  }
+
+  slickInit();
+
+  $(window).resize(function () {
+    slickInit();
+  });
+});
+
+//slick slide nopain
+$(document).ready(function () {
+  $(".nopain-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    swipeToSlide: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
+});
